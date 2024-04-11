45bab41bd7c61377e1891dd58dba5713a0c7608c
Nobert Mumo <mumongungu@gmail.com>
2024-04-11 10:36:22 +0300

    adding mortality reporting and aligning dims

--- models/dmi_data_mart/dim_facility.sql
+++ models/dmi_data_mart/dim_facility.sql
@@ -1,16 +1,21 @@
 with facility_data as (
     select 
         distinct 
-        {{ dbt_utils.surrogate_key( ['sari_data.facility_name']) }} as facility_key,
+        {{ dbt_utils.surrogate_key( ['facility_mapping.facility_name']) }} as facility_key,
+        mfl_code,
         facility_name,
+        facility_name_short,
         longitude,
         latitude
-    from {{ ref('stg_sari_ili') }} as sari_data
+    from {{ ref('facility_mapping') }} as facility_mapping
 
     union 
+
     select 
         'unset' as facility_key,
+        -999 as mfl_code,
         'unset' as facility_name,
+        'unset' as facility_name_short,
         -999 as longitude,
         -999 as latitude
 )
--- models/dmi_reporting/aggregate_moh_505_report.sql
+++ models/dmi_reporting/aggregate_moh_505_report.sql
@@ -1,7 +1,7 @@
 select 
     disease.disease,
     moh_505.gender,
-    age_group_category,
+    moh_505.age_group_category,
     epi_wk.week_number as epi_week,
     epi_wk.year,
     cast(epi_wk.start_of_week as date) as start_of_epi_week,
@@ -14,7 +14,6 @@ select
     moh_505.data_source,
     cast(current_date as date) as load_date
 from {{ref('fct_aggregate_moh_505')}} as moh_505
-left join {{ ref('dim_age_group') }} as age_group on age_group.age_group_key = moh_505.age_group_key
 left join {{ ref('dim_epi_week') }} as epi_wk on epi_wk.epi_week_key = moh_505.epi_week_key
 left join {{ ref('dim_county') }} as county on county.county_key = moh_505.county_key
 left join {{ ref('dim_disease') }} as disease on disease.disease_key = moh_505.disease_key
