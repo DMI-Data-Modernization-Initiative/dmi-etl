fc747b26f5cf3246a91faa8579c7a2370fa3213f
Brian Kariuki <briankariukidev@gmail.com>
2025-07-01 13:41:53 +0300

    update ebridge diarrhoeal model tables

--- models/dmi_reporting/aggregate_moh_505_diarrheal_diseases_report.sql
+++ models/dmi_reporting/aggregate_moh_505_diarrheal_diseases_report.sql
@@ -1,23 +1,76 @@
-with diarrheal_diseases  as (
-	select 
-		sub_county,
-		county,
-        epi_week,
-		year,
-		sum(case when disease = 'Typhoid fever' then indicator_value else 0 end) as typhoid_fever_cases,
-		sum(case when disease = 'Dysentery' then indicator_value else 0 end) as dysentery_cases,
-		sum(case when disease = 'Cholera' then indicator_value else 0 end) as cholera_cases
-	from  {{ ref('aggregate_moh_505_report') }}
-	where disease in ('Typhoid fever', 'Dysentery', 'Cholera')
-		and indicator_description = 'Number of cases reported'
-	group by
-		sub_county,
-		county,
-        epi_week,
-		year
-)
-select 
-	diarrheal_diseases.*,
-	typhoid_fever_cases + dysentery_cases + cholera_cases as total_cases
+with
+    diarrheal_diseases as (
+        select
+            sub_county,
+            county,
+            epi_week,
+            year,
+            concat(year, lpad(epi_week::text, 2, '0')) as year_epi_week,
+            sum(
+                case
+                    when disease = 'Typhoid fever' and age_group_category = '< 5 years'
+                    then indicator_value
+                    else 0
+                end
+            ) as "typhoid<5",
+
+            sum(
+                case
+                    when disease = 'Typhoid fever' and age_group_category = '>= 5 years'
+                    then indicator_value
+                    else 0
+                end
+            ) as "typhoid>5",
+
+            sum(
+                case
+                    when disease = 'Dysentery' and age_group_category = '< 5 years'
+                    then indicator_value
+                    else 0
+                end
+            ) as "dysentery<5",
+
+            sum(
+                case
+                    when disease = 'Dysentery' and age_group_category = '>= 5 years'
+                    then indicator_value
+                    else 0
+                end
+            ) as "dysentery>5",
+
+            sum(
+                case
+                    when disease = 'Cholera' and age_group_category = '< 5 years'
+                    then indicator_value
+                    else 0
+                end
+            ) as "cholera<5",
+
+            sum(
+                case
+                    when disease = 'Cholera' and age_group_category = '>= 5 years'
+                    then indicator_value
+                    else 0
+                end
+            ) as "cholera>5",
+
+            sum(
+                case when disease = 'Typhoid fever' then indicator_value else 0 end
+            ) as typhoid_fever_cases,
+            sum(
+                case when disease = 'Dysentery' then indicator_value else 0 end
+            ) as dysentery_cases,
+            sum(
+                case when disease = 'Cholera' then indicator_value else 0 end
+            ) as cholera_cases
+        from {{ ref("aggregate_moh_505_report") }}
+        where
+            disease in ('Typhoid fever', 'Dysentery', 'Cholera')
+            and indicator_description = 'Number of cases reported'
+        group by sub_county, county, epi_week, year, year_epi_week
+    )
+select
+    diarrheal_diseases.*,
+    typhoid_fever_cases + dysentery_cases + cholera_cases as total_cases
 from diarrheal_diseases
 where typhoid_fever_cases + dysentery_cases + cholera_cases > 0
